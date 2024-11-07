7f6828d19c0d916f8b6336b5de0f86761ae6628c
ebraculj <ela.braculj4@gmail.com>
2024-11-07 16:54:00 +0100

    Dodana programska pozadina za napad sde

--- public/js/script.js
+++ public/js/script.js
@@ -39,3 +39,50 @@
          }
      });
  }
+
+ //lazna baza podataka
+let fakeDatabase = {
+    users: []
+};
+
+//hash sha256
+function hashPassword(password) {
+    // U pravoj aplikaciji koristit ćemo specijaliziranu biblioteku za hashiranje
+    // Ovdje koristimo browser-ovu funkcionalnost za SHA-256
+    return crypto.subtle.digest("SHA-256", new TextEncoder().encode(password)).then(buffer => {
+        let hexArray = Array.from(new Uint8Array(buffer));
+        let hexString = hexArray.map(byte => byte.toString(16).padStart(2, "0")).join("");
+        return hexString;
+    });
+}
+
+//dohvacanje stanja checkboxa i podataka koji su uneseni
+const enableInsecureStorageDBCheckbox = document.getElementById('sdeTrue');
+const userDataForm = document.getElementById('sensitiveData');
+const usernameInputDB = document.getElementById('usernameIn');
+const passwordInputDB = document.getElementById('passIn');
+const dbOutput = document.getElementById('sdeOut');
+
+userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = usernameInputDB.value;
+    const password = passwordInputDB.value;
+    
+    //ako je omogucen napad
+    if (enableInsecureStorageDBCheckbox.checked) {
+        //pohrana samo kao plaintext
+        fakeDatabase.users.push({ username: username, password: password });
+        alert("PODACI NISU ZAŠTIĆENI!\nPodaci su spremljeni kao plaintext.\n" + 
+            "Korisničko ime: " + username + "\n" + 
+            "Lozinka: " + password + "\n"
+        )
+        //dbOutput.innerText = "Podaci pohranjeni u bazu podataka (plaintext)";
+    } else {
+        // hashiranje i pohrana lozinke
+        hashPassword(password).then((hashedPassword) => {
+            fakeDatabase.users.push({ username: username, password: hashedPassword });
+            dbOutput.innerText = "Podaci pohranjeni u bazu podataka (hashirana lozinka)";
+            dbOutput.innerText = "Korisničko ime: " + username + "\nLozinka: " + hashedPassword + "\n"
+        });
+    }
+});
